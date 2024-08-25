e00a711b020bcd59e3664ea57d64d2c3a5a2073b
waleed-saddique <wbs90704@gmail.com>
2024-08-25 18:29:39 +0500

    vercel-update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,22 @@ const page =() => {
   return(
 <div>export default function Home() {
 <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/Waleed-pic.jpg" alt="" width="384" height="512"/>
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/Waleed-pic.jpg"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “This is my first project about vercel today class ,i can adapts any design with the help of Tailwind CSS ,Tailwind css is the only framework that I've seen scale
-        on large teams. It’s easy to customize,
+      “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.
         .”
       </p>
     </blockquote>
     <figcaption className="font-medium">
       <div className="text-sky-500 dark:text-sky-400">
-        Waleed Saddique
+        Sara Dayan
       </div>
       <div className="text-slate-700 dark:text-slate-500">
-        Generative Ai Engineer, Pakistan
+      Staff Engineer, Algolia
       </div>
     </figcaption>
   </div>
